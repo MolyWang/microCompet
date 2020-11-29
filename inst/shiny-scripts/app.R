b4a5c5a158cbdf2c784e204df284392b2df3b06b
Zhuyi Wang <moly.wang@mail.utoronto.ca>
2020-11-29 14:17:31 -0500

    Add dataset ED upload

--- inst/shiny-scripts/app.R
+++ inst/shiny-scripts/app.R
@@ -4,28 +4,63 @@ options(shiny.maxRequestSize = 10*1024^2)
 
 ui <- fluidPage(
 
-  titlePanel("Welcom to microCompet"),
+  titlePanel("Welcom to microCompet: Identify Microbial Competitors for Nutrition"),
 
   sidebarLayout(
+
     sidebarPanel(
+
+      # ============ Ask for Info about the genome of interest ============
       textInput(inputId = "genomeName",
                 label = "Enter the name of your microbe."),
 
       fileInput(inputId = "genome",
                 label = "Upload the annotated genbank file for your microbe of interest.",
-                accept = c(".gb", ".gbk")
-      ),
+                accept = c(".gb", ".gbk")),
 
+
+      # ============ Ask for the dataset ED ============
+      selectInput(inputId = "selectED",
+                  label = "Select an ED dataset",
+                  choices = list("Use built-in EnzymeDistribution", "Upload my own")),
+
+      uiOutput("selectEDFile"),
+
+      actionButton(inputId = "preview", label = "Preview ED"),
+
+
+      # ============ Ask for the dataset ER ============
+
+
+      # ============ Button to run microCompet functions ============
+      # extractCarboGenes
       actionButton(inputId = "extractCarboGenes",
                    label = "Extract Carbo Genes"),
 
-      conditionalPanel(condition="$('html').hasClass('shiny-busy')",
-                       tags$div("Ahh, I am running! Give me some time ~",id="loadmessage"))
+      # constructFullNetwork
+
+      # overallSimilarity
+
+      # competMicrobiota
+
+
+      # ============ Show running message ============
+      conditionalPanel(condition = "$('html').hasClass('shiny-busy')",
+                       tags$div("Ahh, I am running! Give me some time ~",id="loadmessage")),
 
     ),
 
     mainPanel(
+
+      tags$strong("Functions are not run until you select the output panel.",
+                  style = "color:orange"),
+
       tabsetPanel(
+
+        tabPanel("Dataset (ER & ED) Preview and Check",
+                 textOutput(outputId = "datasetCheckResult"),
+                 tableOutput(outputId = "previewPanel")),
+
         tabPanel("Extract Sugar Degradation Genes",
                  tableOutput(outputId = "carboGenes")
         )
@@ -36,11 +71,41 @@ ui <- fluidPage(
 
 server <- function(input, output) {
 
+  # ============ Determine ED ============
+  needUploadED <- reactive({
+    input$selectED == "Upload my own"
+  })
+
+  output$selectEDFile <- renderUI({
+    if (needUploadED()) {
+      fileInput(inputId = "ED",
+                label = "Upload your ED dataset here.")
+    }
+  })
+
+  ED <- reactive({
+    if (needUploadED()) {
+      read.delim2(input$ED$datapath)
+    } else {
+      microCompet::EnzymeDistribution
+    }
+  })
+
+  EDPreview <- eventReactive(input$preview, {
+    ED()
+  })
+
+  output$previewPanel <- renderTable({
+    EDPreview()
+  })
+
+
+
 
   # ============ For extractCarboGenes ============
   carboGenes <- eventReactive(eventExpr = input$extractCarboGenes, {
     microCompet::extractCarboGenes(input$genome$datapath,
-                                   microCompet::EnzymaticReactions$Gene)
+                                   ED()$Gene)
   })
 
   genomeName <- reactive({
@@ -58,8 +123,9 @@ server <- function(input, output) {
 
 
 
-
 }
 
 shinyApp(ui = ui, server = server)
 
+
+#[END]
